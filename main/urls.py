e00ce7d19efda8b544e3f2faf929ded1bf0cc859
Nur Hafsari Setyorini <nur.hafsari@ui.ac.id>
2020-10-07 19:59:56 +0700

    first trial

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('profile', views.profile, name="profile")
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+def profile(request):
+    return render(request, 'main/index.html')
