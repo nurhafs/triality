bef6e65baea7d29d0c120d91d3729ce6787c3c00
Nur Hafsari Setyorini <nur.hafsari@ui.ac.id>
2020-10-09 12:32:29 +0700

    changed views.py

--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, 'main/home.html')
+#def home(request):
+ #   return render(request, 'main/home.html')
 
 def main_page(request):
     return render(request, 'main/main_page.html')
